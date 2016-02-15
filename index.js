158d9ed91c3f786a51d6966246fc5a046cf2d4f0
Hugo Santos <hugoasantos@gmail.com>
2016-02-15 02:59:26 -0200

    Spacing adjust

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ module.exports.mark = function(start, comment) {
 		var nanoseconds = total[1];
 
 		// Calculate time
-    	var microseconds = parseInt(nanoseconds/1000);
-    	var milliseconds = parseInt(microseconds/1000);
+		var microseconds = parseInt(nanoseconds/1000);
+		var milliseconds = parseInt(microseconds/1000);
 		var sec_num = parseInt(seconds, 10);
-	    var hours   = Math.floor(sec_num / 3600);
-	    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-	    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+		var hours   = Math.floor(sec_num / 3600);
+		var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+		var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-	    // Format time	    
-	    var hours   = (10 > hours)?   "0"+hours   : hours;
-	    var minutes = (10 > minutes)? "0"+minutes : minutes;
-	    var seconds = (10 > seconds)? "0"+seconds : seconds;
-	    var time    = hours +':'+ minutes +':'+ seconds +'.'+ milliseconds +' ms';
+		// Format time	    
+		var hours   = (10 > hours)?   "0"+hours   : hours;
+		var minutes = (10 > minutes)? "0"+minutes : minutes;
+		var seconds = (10 > seconds)? "0"+seconds : seconds;
+		var time    = hours +':'+ minutes +':'+ seconds +'.'+ milliseconds +' ms';
 
 		// Format comment
 		comment = (comment)? " - "+ comment : "";
